d02fd66049c968414ec08ca66f4d371284acd738
Per Lindgren <per.lindgren@ltu.se>
2024-09-20 12:36:59 +0200

    wip

---
+++ README.md
@@ -0,0 +1,3 @@
+# Phase-Vocoder
+
+A library based on original code from https://github.com/NathanRoyer/pitch_shift, aiming to provide time stretching.
--- octave/plot_fft.m
+++ octave/plot_fft.m
@@ -1,14 +1,24 @@
 fs = 1000;
 x = 1:fs;
+x2 = 1:fs * 2;
 f = 20;
 data = sin(2 * pi * f * x / fs);
 
 fft_data = fft(data);
 
-ifft_data = ifft(fft_data);
+fft_data_stretch = fft_data;
+
+for i = 1:fs
+    fft_data_stretch(i) = fft_data(round (0.5 + i / 2));
+endfor
+
+ifft_data = ifft(fft_data, fs);
+
+ifft_data2 = ifft(fft_data_stretch, fs * 2);
 
 % figure(1);
-plot(x, data(x), '-xo', x, data(1 + rem(x, fs), '-xr');
+plot(x, ifft_data(x), '-b', x2, ifft_data2(x2), '-r');
+%plot(data, '-b', ifft_data, '-r');
 % hold on
 % plot(x, data(x + 17), '-xr');
 % hold off

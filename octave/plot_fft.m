6b42bd8df270a8a1ec3a592b4acf142b3ffd2651
Per Lindgren <per.lindgren@ltu.se>
2024-09-20 10:56:35 +0200

    test

--- octave/plot_fft.m
+++ octave/plot_fft.m
@@ -5,4 +5,21 @@ data = sin(2 * pi * f * x / fs);
 
 fft_data = fft(data);
 
-plot(x, fft_data);
+ifft_data = ifft(fft_data);
+
+% figure(1);
+plot(x, data(x), '-xo', x, data(1 + rem(x, fs), '-xr');
+% hold on
+% plot(x, data(x + 17), '-xr');
+% hold off
+% title("data");
+% legend("legend data");
+
+% figure(2);
+% plot(x, ifft_data(x));
+% title("ifft_daa");
+% legend("legend ifft_data");
+
+% plot(x, data);
+
+% plot(x, ifft_data);

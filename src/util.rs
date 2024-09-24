36236405daf0f91c7924b8ba275a24df7835162f
Per Lindgren <per.lindgren@ltu.se>
2024-09-24 14:57:23 +0200

    works but ...

--- src/util.rs
+++ src/util.rs
@@ -16,7 +16,7 @@ pub fn gen_sin(f: f32, fs: usize, len: usize) -> Vec<f32> {
         .collect()
 }
 
-pub fn write_csv<T>(data: Vec<T>, path: &impl AsRef<Path>)
+pub fn write_csv<T>(data: &Vec<T>, path: &impl AsRef<Path>)
 where
     T: Display,
 {
@@ -34,7 +34,7 @@ mod test {
     fn test_gen_sin() {
         let sin = gen_sin(10_000.0, 48000, 1000);
 
-        write_csv(sin, &"sin_f32.data");
+        write_csv(&sin, &"sin_f32.data");
     }
 
     #[test]
@@ -42,29 +42,63 @@ mod test {
         const FS: usize = 1000;
         let f = 100.0;
 
-        let mut data = gen_sin(f, FS, FS);
+        let mut sin_100 = gen_sin(f, FS, FS);
+        write_csv(&sin_100, &"sin_100.data");
 
         let mut planner = RealFftPlanner::new();
         let fft_r2c = planner.plan_fft_forward(FS);
         let mut spectrum = fft_r2c.make_output_vec();
 
-        fft_r2c.process(&mut data, &mut spectrum).unwrap();
+        fft_r2c.process(&mut sin_100, &mut spectrum).unwrap();
 
         println!(
             "data length {}, spectrum length {}",
-            data.len(),
+            sin_100.len(),
             spectrum.len()
         );
 
         let fft_norm: Vec<_> = spectrum.iter().map(|c| c.norm()).collect();
 
+        let mut spectrum2 = fft_r2c.make_output_vec(); // zeroes
+
+        let shift = 0.5;
+        // shift spectrum
+        spectrum2.iter_mut().enumerate().for_each(|(i, bin_out)| {
+            let to_bin: f32 = i as f32 * shift;
+            let left = to_bin.floor();
+            let right = (to_bin + 1.0).floor();
+            let left_weight = 1.0 - (to_bin - left).abs();
+            let right_weight = 1.0 - (to_bin - right).abs();
+            // println!("i {}, to_bin {}", i, to_bin);
+            let left_sample = if let Some(r) = spectrum.get(left as usize) {
+                r.to_owned()
+            } else {
+                Complex::zero()
+            };
+            let right_sample = if let Some(r) = spectrum.get(right as usize) {
+                r.to_owned()
+            } else {
+                Complex::zero()
+            };
+
+            *bin_out = left_sample * left_weight + right_sample * right_weight;
+
+            // println!(
+            //     "i {}, to_bin {}, left {}, right {}, left_weight {}, right_weight {}",
+            //     i, to_bin, left, right, left_weight, right_weight
+            // );
+        });
+
+        let fft_norm2: Vec<_> = spectrum2.iter().map(|c| c.norm()).collect();
+
         let ifft_cr = planner.plan_fft_inverse(FS);
         let mut ifft_data = ifft_cr.make_output_vec();
 
-        ifft_cr.process(&mut spectrum, &mut ifft_data).unwrap();
+        ifft_cr.process(&mut spectrum2, &mut ifft_data); // .unwrap();
 
-        write_csv(fft_norm, &"spectrum.data");
-        write_csv(ifft_data, &"ifft.data");
+        write_csv(&fft_norm, &"spectrum.data");
+        write_csv(&fft_norm2, &"spectrum2.data");
+        write_csv(&ifft_data, &"ifft.data");
     }
 }
 
